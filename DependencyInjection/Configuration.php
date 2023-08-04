18b47f138f5978bb3c793a09405ca140ce930539
lcp0578 <lcp0578@gmail.com>
2023-08-04 18:08:45 +0800

    upgrade symfony 5.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('kit_generator');
+        $treeBuilder = new TreeBuilder('kit_generator');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
